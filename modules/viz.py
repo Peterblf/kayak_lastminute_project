763e3d4c94a7ea2332bf34819fe775a4346e06e4
Peterblf <pierrebrech@icloud.com>
2026-05-22 14:42:21 +0200

    Update viz.py

--- modules/viz.py
+++ modules/viz.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 
 def plot_price_by_destination(summary_df, outpath="price_by_destination.png"):
 
-    if summary_df is None or summary_df.empty:  # verif que le dataframe != vide
+    if summary_df is None or summary_df.empty: 
         print("Aucune donnée pour tracer le graphique.")
         return
 
     destinations = summary_df.index.tolist()
     prices = summary_df["average_price"].tolist()
 
-    # création du graph
     fig, ax = plt.subplots(figsize=(6, 4))
     ax.bar(destinations, prices)
     ax.set_xlabel("Destination")
@@ -18,6 +17,5 @@ def plot_price_by_destination(summary_df, outpath="price_by_destination.png"):
     ax.set_title("Prix moyen par destination (offres filtrées)")
     plt.tight_layout()
 
-    # save
     fig.savefig(outpath)
     plt.close(fig)
